be360dc72f9e257498542ecd8d20f6b4a3e64671
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 18:49:13 -0500

    finish print thrice exercise

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -7,6 +7,16 @@ public class PrintThrice {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.print("Give a word: ");
+        String word = scanner.nextLine();
+        
+        repeatString(word, 3);
+    }
+    
+    public static void repeatString(String str, int timesToRepeat) {
+        for (int i = 0; i < timesToRepeat; i++) {
+            System.out.print(str);
+        }
+        System.out.println("");
     }
 }
