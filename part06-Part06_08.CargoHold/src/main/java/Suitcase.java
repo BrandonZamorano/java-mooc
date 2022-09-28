999c1e333ffd131e73ecde3f6c376f9b605c7d9e
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 16:34:29 -0500

    finish part2 suitcase class

---
+++ part06-Part06_08.CargoHold/src/main/java/Suitcase.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author brandonwebdev
+ */
+public class Suitcase {
+
+    private ArrayList<Item> items;
+    private int maxWeight;
+
+    public Suitcase(int maxWeight) {
+        this.items = new ArrayList<>();
+        this.maxWeight = maxWeight;
+    }
+
+    public void addItem(Item item) {
+        int newWeight = getWeight() + item.getWeight();
+        if (newWeight > maxWeight) {
+            return;
+        }
+
+        items.add(item);
+    }
+
+    public int getWeight() {
+        int totalWeight = 0;
+
+        for (Item item : items) {
+            totalWeight += item.getWeight();
+        }
+
+        return totalWeight;
+    }
+
+    public String toString() {
+        return items.size() + " items (" + getWeight() + "kg)";
+    }
+
+}
