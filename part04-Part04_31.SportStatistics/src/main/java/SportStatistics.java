a1e338e8a2a78a616e6d9cc74a4fa1a5a61686a3
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 09:27:43 -0500

    finish part 1 games played

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -6,7 +6,28 @@ import java.util.Scanner;
 public class SportStatistics {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("File:");
+        String fileName = scanner.nextLine();
+        
+        System.out.println("Team:");
+        String teamName = scanner.nextLine();
+        
+        try (Scanner fileReader = new Scanner(Paths.get(fileName))) {
+            int gameCount = 0;
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                
+                if (line.contains(teamName)) {
+                    gameCount++;
+                }
+                
+            }
+            System.out.println("Games: " + gameCount);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
