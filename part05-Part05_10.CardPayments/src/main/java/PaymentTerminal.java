748718cc8d7cf6bfdb58eaff54a35259aebcbc52
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 11:49:59 -0500

    finish part 2 payment terminal and cash

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,42 @@
 
 public class PaymentTerminal {
 
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
+        double affordableMealCost = 2.50;
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+
+        if (payment >= affordableMealCost) {
+            this.money += affordableMealCost;
+            this.affordableMeals++;
+            return payment - affordableMealCost;
+        }
+        return payment;
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
+        double heartyMealCost = 4.30;
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
-    }
 
+        if (payment >= heartyMealCost) {
+            this.money += heartyMealCost;
+            this.heartyMeals++;
+            return payment - heartyMealCost;
+        }
+        return payment;
+    }
 
     @Override
     public String toString() {
