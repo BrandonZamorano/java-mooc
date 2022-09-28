dbc4c409b08fa8e5f7fc0c1180d8d0ccad7db242
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 19:15:51 -0500

    finish name of the oldest exercise

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,6 +5,27 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String oldestName = "";
+        int oldestAge = -1;
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.length() == 0) {
+                break;
+            }
+            
+            String[] pieces = input.split(",");
+            String name = pieces[0];
+            int age = Integer.valueOf(pieces[1]);
+            
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestName = name;
+            }
+            
+        }
+        
+        System.out.println("Name of the oldest: " + oldestName);
 
 
     }
