01c27a8871388688868424ee57089e35bf41058d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 19:29:10 -0500

    finish personal details exercise

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,31 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String longestName = "";
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.length() == 0) {
+                break;
+            }
+            
+            String[] pieces = input.split(",");
+            String name = pieces[0];
+            int year = Integer.valueOf(pieces[1]);
+            
+            if (name.length() > longestName.length()) {
+                longestName = name;
+            }
+            
+            count++;
+            sum += year;
+            
+        }
+        
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + (1.0 * sum / count));
 
     }
 }
