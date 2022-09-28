2d9466fe53822af8187336acb6d9debffbdeaccc
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 20:50:27 -0500

    update Person class

--- part01-Part01_01.Sandbox/src/main/java/Person.java
+++ part01-Part01_01.Sandbox/src/main/java/Person.java
@@ -11,4 +11,13 @@
 public class Person {
     private String name;
     private int age;
+    
+    public Person(String initialName) {
+        this.name = initialName;
+        this.age = 0;
+    }
+    
+    public void printPerson() {
+        System.out.println(this.name + ", age " + this.age + " years");
+    }
 }
--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,6 +3,12 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
-        System.out.println("Hello, world!");
+        Person ada = new Person("Ada");
+        Person antti = new Person("Antti");
+        Person martin = new Person("Martin");
+
+        ada.printPerson();
+        antti.printPerson();
+        martin.printPerson();
     }
 }
