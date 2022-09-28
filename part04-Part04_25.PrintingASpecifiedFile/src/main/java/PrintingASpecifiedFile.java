d2c421ac0d5699a3b22d0c0e9a58c8bd3dd3979a
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 08:56:25 -0500

    finish printing a specified file exercise

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -7,5 +7,19 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Which file should have its contents printed");
+        String fileName = scanner.nextLine();
+
+        scanner.close();
+
+        try ( Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                String row = fileScanner.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
