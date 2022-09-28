277324e51a3381bcde788270886f952416072a57
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 19:01:11 -0500

    finish line by line exercise

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,21 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+
+            String word = scanner.nextLine();
+
+            if (word.length() == 0) {
+                break;
+            }
+
+            String[] pieces = word.split(" ");
+
+            for (String piece : pieces) {
+                System.out.println(piece);
+
+            }
+        }
 
     }
 }
