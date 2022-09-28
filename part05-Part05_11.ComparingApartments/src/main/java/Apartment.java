31c7123544ec376b6b111908df075ace43e3b0eb
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 12:09:10 -0500

    finish comparing apartments exercise

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,28 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared) {
+        return this.squares > compared.squares;
+    }
+
+    public int priceDifference(Apartment compared) {
+        int priceDifference = (this.squares * this.princePerSquare) - (compared.squares * compared.princePerSquare);
+
+        if (priceDifference < 0) {
+            priceDifference *= -1;
+        }
+
+        return priceDifference;
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        int priceDifference = (this.squares * this.princePerSquare) - (compared.squares * compared.princePerSquare);
+
+        if (priceDifference > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
