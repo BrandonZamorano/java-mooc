cd696d1a749f9be491b71e5ad377945597afe829
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 21:06:42 -0500

    update Person class

--- part01-Part01_01.Sandbox/src/main/java/Person.java
+++ part01-Part01_01.Sandbox/src/main/java/Person.java
@@ -11,13 +11,23 @@
 public class Person {
     private String name;
     private int age;
-    
+
     public Person(String initialName) {
-        this.name = initialName;
         this.age = 0;
+        this.name = initialName;
     }
-    
+
     public void printPerson() {
         System.out.println(this.name + ", age " + this.age + " years");
+    }
+
+    public void growOlder() {
+        if (this.age < 30) {
+            this.age = this.age + 1;
+        }
+    }
+    // the added method
+    public int returnAge() {
+        return this.age;
     }
 }
