753ce9148888ba4a45458d7be8e4db553e193d88
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 09:21:19 -0500

    finish storing records exercise

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -25,6 +25,19 @@ public class StoringRecords {
 
         // Write here the code for reading from file
         // and printing the read records
+        try ( Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                persons.add((new Person(name, age)));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
         return persons;
 
     }
