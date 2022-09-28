40d4b435778c9098240f7b3c4bd1d52ea2c1f91b
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 08:32:03 -0500

    finish books exercise

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -1,12 +1,46 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Book> books = new ArrayList();
+
+        while (true) {
+            System.out.print("Title: ");
+            String title = scanner.nextLine();
+
+            if (title.length() == 0) {
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+
+            System.out.print("Publication year: ");
+            int yearPublished = Integer.valueOf(scanner.nextLine());
+
+            books.add(new Book(title, pages, yearPublished));
+        }
+        System.out.println();
+
+        System.out.print("WHat information will be printed? ");
+        String searchValue = scanner.nextLine();
+
+        for (Book book : books) {
+
+            if (searchValue.equals("everything")) {
+                System.out.println(book);
+            } else if (searchValue.equals("name")) {
+                System.out.println(book.getTitle());
+            }
+        }
 
     }
 }
