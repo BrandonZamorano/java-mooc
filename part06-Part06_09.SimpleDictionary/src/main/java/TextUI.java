3e3dfc66f6b326388eb098cc5b8364f87b606f4f
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 18:44:37 -0500

    finish part1 starting and stopping the ui

---
+++ part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author brandonwebdev
+ */
+public class TextUI {
+
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+
+    public void start() {
+
+        while (true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("end")) {
+                break;
+            }
+            
+            System.out.println("Unkown command");
+
+        }
+        System.out.println("Bye bye!");
+    }
+
+}
