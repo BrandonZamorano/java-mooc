6ff54d5979aafd1dcbd94c90e578d4c12aac3cf7
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-27 19:04:53 -0500

    finish av club exercise

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,6 +5,22 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            String word = scanner.nextLine();
+            
+            if (word.length() == 0) {
+                break;
+            }
+            
+            String[] pieces = word.split(" ");
+            
+            for (String piece : pieces) {
+                if (piece.contains("av")) {
+                    System.out.println(piece);
+                }
+            }
+        }
 
 
     }
