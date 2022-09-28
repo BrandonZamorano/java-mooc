eb2ae557ff8eaee7e2235e5399f11536d0208e73
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 08:04:11 -0500

    finish part3 non-negative balance

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -5,14 +5,13 @@ public class MainProgram {
         // Scanner scanner = new Scanner(System.in);
         
         // Here you can write code to test that PaymentCard works as intended
-                PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
-
-        card.eatAffordably();
+                PaymentCard card = new PaymentCard(5);
         System.out.println(card);
 
         card.eatHeartily();
-        card.eatAffordably();
+        System.out.println(card);
+
+        card.eatHeartily();
         System.out.println(card);
 
         // be sure to erase the extra code for the last part of the exercise!
--- part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -9,22 +9,28 @@
  * @author brandonwebdev
  */
 public class PaymentCard {
+
     private double balance;
-    
+
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
     }
-    
+
     public String toString() {
         return "The card has a balance of " + this.balance + " euros";
     }
-    
+
     public void eatAffordably() {
-        this.balance -= 2.60;
+
+        if (this.balance - 2.60 >= 0) {
+            this.balance -= 2.60;
+        }
     }
-    
+
     public void eatHeartily() {
-        this.balance -= 4.60;
+        if (this.balance - 4.60 >= 0) {
+            this.balance -= 4.60;
+        }
     }
-    
+
 }
