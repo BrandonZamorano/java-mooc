850fc9b2cb7454a282d2b967f56809322ce0148d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 15:38:30 -0500

    finish part1 adding to the stack and checking emptiness

---
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brandonwebdev
+ */
+public class Stack {
+    private ArrayList<String> list;
+    
+    public Stack() {
+        this.list = new ArrayList<>();
+    }
+    
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+    
+    public void add (String value) {
+        list.add(value);
+    }
+    
+    public ArrayList<String> values() {
+        return list;
+    }
+    
+}
