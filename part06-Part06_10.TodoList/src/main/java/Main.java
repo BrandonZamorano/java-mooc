2312ab2eac6a54134619f64e368929df3957dcaa
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-28 19:02:32 -0500

    finish part1 todolist

--- part06-Part06_10.TodoList/src/main/java/Main.java
+++ part06-Part06_10.TodoList/src/main/java/Main.java
@@ -5,12 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Here you can try out the combined functionality of your classes
         TodoList list = new TodoList();
-        Scanner scanner = new Scanner(System.in);
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
 
-        UserInterface ui = new UserInterface(list, scanner);
-        ui.start();
+        list.print();
+        list.remove(2);
 
+        System.out.println();
+        list.print();
+
+        // Here you can try out the combined functionality of your classes
+//        TodoList list = new TodoList();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        UserInterface ui = new UserInterface(list, scanner);
+//        ui.start();
     }
 }
