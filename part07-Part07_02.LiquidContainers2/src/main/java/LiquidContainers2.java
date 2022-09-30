8f743d485611dd82fdd759314d207faf1843b99c
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-30 10:50:44 -0500

    finish part2 functionality

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -2,34 +2,54 @@
 import java.util.Scanner;
 
 public class LiquidContainers2 {
-
+    
     public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//
-//
-//        while (true) {
-//            System.out.print("> ");
-//
-//            String input = scan.nextLine();
-//            if (input.equals("quit")) {
-//                break;
-//            }
-//
-//        }
+        
+        Scanner scan = new Scanner(System.in);
 
-        Container container = new Container();
-        System.out.println(container);
-
-        container.add(50);
-        System.out.println(container);
-        System.out.println(container.contains());
-
-        container.remove(60);
-        System.out.println(container);
-
-        container.add(200);
-        System.out.println(container);
-
+//        int firstContainer = 0;
+//        int secondContainer = 0;
+//        int CONTAINER_MAX = 100;
+        Container firstContainer = new Container();
+        Container secondContainer = new Container();
+        
+        while (true) {
+            System.out.println("");
+            //Print the contents of both containers
+            System.out.println("First: " + firstContainer);
+            System.out.println("Second: " + secondContainer);
+            
+            String input = scan.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if (command.equals("add")) {
+                // handle add command
+                
+                firstContainer.add(amount);
+            } else if (command.equals("move")) {
+                
+                int amountToMove = amount;
+                
+                if (firstContainer.contains() - amountToMove < 0) {
+                    // amount to move is greater than amount in first container
+                    amountToMove = firstContainer.contains();
+                }
+                
+                firstContainer.remove(amountToMove);
+                secondContainer.add(amountToMove);
+                
+            } else if (command.equals("remove")) {
+                secondContainer.remove(amount);
+            }
+            
+        }
+        
     }
-
+    
 }
