22a60404af8bd8036e448fe41ff905e9ab8b3e3d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-30 11:01:09 -0500

    finish part 1 finding the smallest value

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -5,5 +5,17 @@ public class MainProgram {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
     }
+    
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        
+        for (int number : array) {
+            if (number < smallest) {
+                smallest = number;
+            }
+        }
+        
+        return smallest;
+    }
 
 }
