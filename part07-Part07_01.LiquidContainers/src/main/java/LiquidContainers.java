b2206b57a495320bb17bcf66fc8a6e7285e2cf61
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-30 09:08:01 -0500

    finish part1 adding

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -6,16 +6,44 @@ public class LiquidContainers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int firstContainer = 0;
+        int secondContainer = 0;
+        int CONTAINER_MAX = 100;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("");
+//Print the contents of both containers
+            System.out.println("First: " + firstContainer + "/" + CONTAINER_MAX);
+            System.out.println("Second: " + secondContainer + "/" + CONTAINER_MAX);
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
 
-        }
-    }
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
 
+            if (command.equals("add")) {
+                // handle add command
+
+                if (amount < 0) {
+                    continue;
+                }
+
+                int newAmount = firstContainer + amount;
+
+                if (newAmount > CONTAINER_MAX) {
+                    firstContainer = CONTAINER_MAX;
+                    continue;
+                }
+                
+                firstContainer += amount;
+
+            }
+
+        }
+
+    }
 }
