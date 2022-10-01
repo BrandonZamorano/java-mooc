109bfcebb6cd8f45e0c99f952f31934bea2884b2
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-01 17:03:17 -0500

    finish part4 a box is packable too

--- mooc-java-programming-ii/part09-Part09_06.InterfaceInABox/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_06.InterfaceInABox/src/main/java/Main.java
@@ -4,15 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Box box = new Box(10);
 
-        box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+//        box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
         box.add(new Book("Robert Martin", "Clean Code", 1));
         box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
 
-        box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
-        box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
-        box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+        Box newBox = new Box(10);
+        newBox.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
+        newBox.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
+        newBox.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+        
+        newBox.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+        newBox.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+        newBox.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+        newBox.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+        newBox.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+        
+        box.add(newBox);
 
         System.out.println(box);
+        System.out.println(newBox);
 
     }
 
