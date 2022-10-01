d14a1f91498033dfbcce10e54ccaf301d67c4460
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-01 12:23:01 -0500

    finish part3 expanded vehicle registry

--- mooc-java-programming-ii/part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
+++ mooc-java-programming-ii/part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /*
@@ -6,37 +7,55 @@ import java.util.HashMap;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author brandonwebdev
  */
 public class VehicleRegistry {
+
     private HashMap<LicensePlate, String> registry;
-    
+
     public VehicleRegistry() {
         this.registry = new HashMap<>();
     }
-    
+
     public boolean add(LicensePlate licensePlate, String owner) {
         if (!registry.getOrDefault(licensePlate, "").isEmpty()) {
             return false;
         }
-        
+
         registry.put(licensePlate, owner);
         return true;
     }
-    
+
     public String get(LicensePlate licensePlate) {
         return registry.get(licensePlate);
     }
-    
+
     public boolean remove(LicensePlate licensePlate) {
         if (!registry.containsKey(licensePlate)) {
             return false;
         }
-        
+
         registry.remove(licensePlate);
         return true;
+    }
+
+    public void printLicensePlates() {
+        for (LicensePlate plate : registry.keySet()) {
+            System.out.println(plate);
+        }
+    }
+
+    public void printOwners() {
+        ArrayList<String> ownersAlreadyPrinted = new ArrayList<>();
+
+        for (String owner : registry.values()) {
+            if (ownersAlreadyPrinted.contains(owner)) {
+                continue;
+            }
+            System.out.println(owner);
+            ownersAlreadyPrinted.add(owner);
+        }
     }
 }
