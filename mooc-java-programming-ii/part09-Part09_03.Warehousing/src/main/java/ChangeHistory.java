fa078343291bbbac1d8eb56db906cca5d0a84dd8
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-01 14:23:56 -0500

    finish part change history step2

--- mooc-java-programming-ii/part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
+++ mooc-java-programming-ii/part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
@@ -6,27 +6,74 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author brandonwebdev
  */
 public class ChangeHistory {
+
     private ArrayList<Double> history;
-    
+
     public ChangeHistory() {
         this.history = new ArrayList<>();
     }
-    
+
     public void add(double status) {
         history.add(status);
     }
-    
+
     public void clear() {
         history.clear();
     }
-    
+
     public String toString() {
         return history.toString();
     }
+
+    public double maxValue() {
+        if (history.isEmpty()) {
+            return 0;
+        }
+
+        double max = history.get(0);
+
+        for (double value : history) {
+            if (value > max) {
+                max = value;
+            }
+        }
+
+        return max;
+    }
+
+    public double minValue() {
+        if (history.isEmpty()) {
+            return 0;
+        }
+
+        double min = history.get(0);
+
+        for (double value : history) {
+            if (value < min) {
+                min = value;
+            }
+        }
+
+        return min;
+    }
+
+    public double average() {
+        if (history.isEmpty()) {
+            return 0;
+        }
+
+        double sum = 0;
+
+        for (double value : history) {
+            sum += value;
+        }
+
+        return sum / history.size();
+    }
+
 }
