8741b625925bf816aedbdcf8d7ca43b84aad7e99
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-01 20:11:07 -0500

    finish limited numbers exercise

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
@@ -7,5 +8,24 @@ public class LimitedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> inputs = new ArrayList<>();
+
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == -1) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        if (inputs.isEmpty()) {
+            return;
+        }
+
+        inputs.stream()
+                .filter(n -> n >= 1 && n <= 5)
+                .forEach(System.out::println);
     }
 }
