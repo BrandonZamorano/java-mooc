8c5f6aaec56499bb20f4615b46d8219ebba6c313
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-01 19:46:19 -0500

    finish positive numbers exercise

--- mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -10,5 +10,11 @@ public class PositiveNumbers {
         // test your method here
 
     }
+    
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n > 0)
+                .collect(Collectors.toList());
+    }
 
 }
