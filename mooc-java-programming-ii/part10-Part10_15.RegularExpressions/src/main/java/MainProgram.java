3aabd120b506cef7b634f40af46f5458a6fdf71d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-02 19:12:30 -0500

    finish regular expressions exercise

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,26 @@
 
+import java.util.Scanner;
+
+
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        Checker checker = new Checker();
+        
+        StringBuilder stringBuilder = new StringBuilder("The form is ");
+        if (checker.timeOfDay(input)) {
+            stringBuilder.append("correct.");
+        } else {
+            stringBuilder.append("incorrect.");
+        }
+        
+        System.out.println(stringBuilder);
     }
 }
