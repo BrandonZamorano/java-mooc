0a2e19a89c2a5960b076da09518f73b266985981
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-02 21:59:22 -0500

    finish part4 comparing hands

--- mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Hand.java
+++ mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -8,27 +8,33 @@ import java.util.List;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author brandonwebdev
  */
-public class Hand {
+public class Hand implements Comparable<Hand> {
+
     private List<Card> cardsInHand;
-    
+
     public Hand() {
         this.cardsInHand = new ArrayList<>();
     }
-    
+
     public void add(Card card) {
         this.cardsInHand.add(card);
     }
-    
+
     public void print() {
         cardsInHand.forEach(card -> System.out.println(card));
     }
-    
+
     public void sort() {
         Collections.sort(cardsInHand);
+    }
+
+    @Override
+    public int compareTo(Hand otherHand) {
+        return this.cardsInHand.stream()
+                .mapToInt(c -> c.getValue()).sum() - otherHand.cardsInHand.stream().mapToInt(c -> c.getValue()).sum();
     }
 }
