b3ef404a3f64f94723b0968468cc7c05667ef101
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-02 21:39:26 -0500

    finish part2 hand

---
+++ mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brandonwebdev
+ */
+public class Hand {
+    private List<Card> cardsInHand;
+    
+    public Hand() {
+        this.cardsInHand = new ArrayList<>();
+    }
+    
+    public void add(Card card) {
+        this.cardsInHand.add(card);
+    }
+    
+    public void print() {
+        cardsInHand.forEach(card -> System.out.println(card));
+    }
+}
