dfd797f68587fb0c28bfbb7e4584338500338eac
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-08 13:01:23 -0500

    finish array as string exercise

--- mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -3,6 +3,27 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your method here
+
+        int[][] matrix = {
+            {3, 2, 7, 6},
+            {2, 4, 1, 0},
+            {3, 2, 1, 0}
+        };
+
+        System.out.println(arrayAsString(matrix));
+
+    }
+
+    public static String arrayAsString(int[][] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : array) {
+            for (int value : row) {
+                sb.append(value);
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 
 }
