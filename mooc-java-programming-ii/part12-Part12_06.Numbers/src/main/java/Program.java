b7cd6825314113174c5ab412884894dfb322e0ef
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-08 12:22:03 -0500

    finish numbers exercise

--- mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,14 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many numbers should be printed?");
+        int input = Integer.valueOf(scanner.nextLine());
+        Random ladyLuck = new Random();
+        
+        for (int i = 0; i < input; i++) {
+            int randomValue = ladyLuck.nextInt(11);
+            System.out.println(randomValue);
+        }
     }
 
 }
