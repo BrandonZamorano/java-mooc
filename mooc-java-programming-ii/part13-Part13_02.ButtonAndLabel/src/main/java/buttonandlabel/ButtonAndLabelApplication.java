f36cf243340faac4af078369e99e04368a1740e4
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-23 10:07:18 -0500

    finish button and label exercise

--- mooc-java-programming-ii/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ mooc-java-programming-ii/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,32 @@
 package buttonandlabel;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndLabelApplication {
+
+public class ButtonAndLabelApplication extends Application {
 
 
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("Hello");
+        Label text = new Label("Label");
+        
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(text);
+        componentGroup.getChildren().add(button);
+        Scene scene = new Scene(componentGroup);
+        window.setTitle("Button and Label");
+        window.setScene(scene);
+        window.show();
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndLabelApplication.class);
     }
 
 }
