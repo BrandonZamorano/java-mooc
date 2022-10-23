be9c7a1c3f69864c3373c437f750a3bd2cfe94c2
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-23 10:29:11 -0500

    finish text statistics exercise

--- mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ mooc-java-programming-ii/part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,10 +1,37 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+public class TextStatisticsApplication extends Application {
 
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        TextArea textArea = new TextArea();
+        layout.setCenter(textArea);
+        
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+        hbox.getChildren().add(new Label("Letters: 0"));
+        hbox.getChildren().add(new Label("Words: 0"));
+        hbox.getChildren().add(new Label("The longest word is:"));
+        
+        layout.setBottom(hbox);
+        Scene view = new Scene(layout);
+        
+        window.setTitle("Text Statistics");
+        window.setScene(view);
+        window.show();
+    }
 
     public static void main(String[] args) {
+        launch(TextStatisticsApplication.class);
         System.out.println("Hello world!");
     }
 
