a0d8a7a6c84860fbb149500736826258c81a9a66
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-23 10:46:10 -0500

    finish notifier exercise

--- mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,10 +1,39 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
 
+public class NotifierApplication extends Application{
+
+    @Override
+    public void start(Stage window) {
+        VBox layout = new VBox();
+        TextField textField = new TextField();
+        Label textLabel = new Label();
+        Button button = new Button("Update");
+        
+        
+        button.setOnAction((event) -> {
+        textLabel.setText(textField.getText());
+        });
+        
+        
+        layout.getChildren().addAll(textField, button, textLabel);
+        
+        Scene view = new Scene(layout);
+        window.setTitle("Notifier");
+        window.setScene(view);
+        window.show();
+    }
 
     public static void main(String[] args) {
+        launch(NotifierApplication.class);
         System.out.println("Hello world!");
     }
 
