e2c36aa5c0a563f8feae1cd114e577e91f7e3c6d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-10-23 10:12:51 -0500

    finish button and textfield exercise

--- mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ mooc-java-programming-ii/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,31 @@
 package buttonandtextfield;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndTextFieldApplication {
+
+public class ButtonAndTextFieldApplication extends Application {
 
 
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("I'm a button");
+        TextField textField = new TextField("Text field");
+        
+        FlowPane groupComponent = new FlowPane();
+        groupComponent.getChildren().addAll(button, textField);
+        Scene view = new Scene(groupComponent);
+        window.setTitle("Button and TextField");
+        window.setScene(view);
+        window.show();
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class);
     }
 
 }
